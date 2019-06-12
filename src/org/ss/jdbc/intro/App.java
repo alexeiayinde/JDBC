3a2c7fd950688f98497750940306493a01ee8126
Alexei AYINDE <alexei.ayinde@gmail.com>
2019-06-12 13:03:49 +0200

    Ajout connexion à MySQL

--- src/org/ss/jdbc/intro/App.java
+++ src/org/ss/jdbc/intro/App.java
@@ -1,8 +1,31 @@
 package org.ss.jdbc.intro;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+
 public class App {
 
-	public static void main(String[] args) {
+	public static void main(String[] args) throws ClassNotFoundException {
+
+		Class.forName("com.mysql.cj.jdbc.Driver"); // Ceci est inutile depuis JDBC 4 !
+
+		String url = "jdbc:mysql://localhost:3306/jdbc-book?useSSL=false&useLegacyDatetimeCode=false&serverTimezone=UTC";
+		String user = "root";
+		String mdp = "";
+
+		try {
+			Connection conn = DriverManager.getConnection(url, user, mdp);
+
+			Statement statement = conn.createStatement();
+
+			ResultSet result = statement.executeQuery("SELECT * FROM ?");
+
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
 
 	}
 
