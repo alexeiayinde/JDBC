8a3b075dbb565d37282773fef24761ab5accca9f
Alexei AYINDE <alexei.ayinde@gmail.com>
2019-06-12 17:21:11 +0200

    Ajout 'ResultMetaData'

--- src/org/ss/jdbc/metadata/App.java
+++ src/org/ss/jdbc/metadata/App.java
@@ -4,9 +4,14 @@ import java.sql.Connection;
 import java.sql.DatabaseMetaData;
 import java.sql.DriverManager;
 import java.sql.ResultSet;
+import java.sql.ResultSetMetaData;
 import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.Scanner;
 
 public class App {
+
+	private static Scanner sc = new Scanner(System.in);
 
 	public static void main(String[] args) throws SQLException {
 
@@ -17,8 +22,48 @@ public class App {
 		Connection conn = DriverManager.getConnection(url, user, pwd);
 
 		DatabaseMetaData metaData = conn.getMetaData();
-		ResultSet rs = metaData.getTables(conn.getCatalog(), null, "c%", null);
+		ResultSet rs = metaData.getTables(conn.getCatalog(), null, null, null);
+		System.out.println("***** LISTE DES TABLES DE LA BDD *****");
+		while (rs.next()) {
+			System.out.print(rs.getString("TABLE_NAME") + " - ");
+		}
+		System.out.println();
 
+		String response;
+		Statement statement = conn.createStatement();
+		do {
+			System.out.print("Entrez le nom de la table : ");
+			response = sc.nextLine();
+
+			if (!response.equals("exit")) {
+				ResultSet result = statement.executeQuery("SELECT * FROM " + response);
+				ResultSetMetaData rsMetaData = result.getMetaData();
+
+				System.out.println("Voici les informations de la table " + response);
+				System.out.println(
+						"\n***************************************************************************************************************************");
+				for (int i = 1; i <= rsMetaData.getColumnCount(); i++) {
+					System.out.printf("%-25s", "| " + rsMetaData.getColumnName(i).toUpperCase() + "["
+							+ rsMetaData.getColumnTypeName(i) + "]");
+				}
+				System.out.println(
+						"\n***************************************************************************************************************************");
+
+				while (result.next()) {
+					for (int i = 1; i <= rsMetaData.getColumnCount(); i++) {
+						System.out.printf("%-25s", "| " + result.getObject(i).toString());
+					}
+					System.out.println(
+							"\n---------------------------------------------------------------------------------------------------------------------------");
+				}
+
+				result.close();
+			}
+
+		} while (!response.equalsIgnoreCase("exit"));
+
+		statement.close();
+		rs.close();
 		conn.close();
 
 	}
